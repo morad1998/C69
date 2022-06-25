247195e94601bc31b1a90e0ea21fda7909e8fe6e
Morad <morad98@gmail.com>
2022-06-25 22:13:42 +0300

    details page

--- frontend/src/components/offersCard.svelte
+++ frontend/src/components/offersCard.svelte
@@ -1,7 +1,7 @@
 <script>
     export let myoffer
 </script>
-<a class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href={`/myoffer/${myoffer.offerId}`} >
+<a class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href={`/myoffer/${myoffer.Promotion_id}`} >
 <img class="object-scale-down h-48 w-96 ..."  src={myoffer.image} alt="offer">
 
 <h2 class="uppercase text-2xl">{myoffer.Promotion_id}.{myoffer.full_descriptin}</h2>
