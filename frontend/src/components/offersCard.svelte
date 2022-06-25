4d1b8c42d492633840a3ea2a3bd9696c72c057c4
Morad <morad98@gmail.com>
2022-06-25 22:10:41 +0300

    ID modification

--- frontend/src/components/offersCard.svelte
+++ frontend/src/components/offersCard.svelte
@@ -4,6 +4,6 @@
 <a class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center" href={`/myoffer/${myoffer.offerId}`} >
 <img class="object-scale-down h-48 w-96 ..."  src={myoffer.image} alt="offer">
 
-<h2 class="uppercase text-2xl">{myoffer.offerId}.{myoffer.full_descriptin}</h2>
+<h2 class="uppercase text-2xl">{myoffer.Promotion_id}.{myoffer.full_descriptin}</h2>
 
 </a>
